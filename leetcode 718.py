3b2d494b76fbe952020e0d0360d2d6bc0b817957
yuanzhg78 <yuanzhg@umich.edu>
2021-08-30 02:07:13 +0800

    add

---
+++ leetcode 718.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findLength(self, A, B):
+        m, n = len(A), len(B)
+        ans = 0
+        dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    ans = max(ans, dp[i][j])
+        return ans
+
+#https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/solution/ni-de-yi-fu-wo-ba-liao-zui-chang-gong-gong-zi-xu-l/
